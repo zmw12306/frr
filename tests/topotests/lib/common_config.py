9d7e2cf7cd4b19637c6285e4ac5c6e234cc99a65
nguggarigoud <nguggarigoud@vmware.com>
2021-10-29 04:14:33 -0700

    tests: Add topotests for MGMT daemon
    1. MGMT daemon support in topotests.
    2. Sanity tests for MGMTd.
    
    Signed-off-by: nguggarigoud <nguggarigoud@vmware.com>

--- tests/topotests/lib/common_config.py
+++ tests/topotests/lib/common_config.py
@@ -103,6 +103,7 @@ DEBUG_LOGS = {
         "debug zebra vxlan",
         "debug zebra nht",
     ],
+    "mgmt": [],
     "ospf": [
         "debug ospf event",
         "debug ospf ism",
@@ -443,6 +444,8 @@ def check_router_status(tgen):
             result = rnode.check_router_running()
             if result != "":
                 daemons = []
+                if "mgmtd" in result:
+                    daemons.append("mgmtd")
                 if "bgpd" in result:
                     daemons.append("bgpd")
                 if "zebra" in result:
@@ -1039,6 +1042,11 @@ def start_topology(tgen):
                 if "ospf6" in val:
                     feature.add("ospf6")
                     break
+
+        # Loading empty mgmtd.conf file to router, to start the mgmtd daemon
+        router.load_config(
+            TopoRouter.RD_MGMTD, "{}/{}/mgmtd.conf".format(tgen.logdir, rname)
+        )
 
         # Loading empty zebra.conf file to router, to start the zebra deamon
         router.load_config(
@@ -2583,7 +2591,7 @@ def create_route_maps(tgen, input_dict, build=False):
                         nexthop = set_data.setdefault("nexthop", None)
                         origin = set_data.setdefault("origin", None)
                         ext_comm_list = set_data.setdefault("extcommunity", {})
-                        metrictype = set_data.setdefault("metric-type", {})
+                        metrictype = set_data.setdefault("metric-type", None)
 
                         # Local Preference
                         if local_preference:
